d4abacc142fa66aa699433333a5f3eb61b3d34e2
lucas-newlands <lucas.newlands@ibm.com>
2023-11-10 14:07:59 -0300

    Initial Commit

---
+++ tiktokClone/tiktokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokCloneApp.swift
+//  tiktokClone
+//
+//  Created by Lucas Newlands on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct tiktokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
